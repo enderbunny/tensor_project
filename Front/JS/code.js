a8adf370835e1d708200a6aad9f836db1ac2a729
elf <tanya_zhukovskaya_96@mail.ru>
2024-04-25 00:53:28 +0500

    aaa

---
+++ Front/JS/code.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Header from './components/head.js'
+
+let App =() => {
+return(
+    <Header />
+);
+}
+export default App;
---
+++ Front/JS/components/head.js
@@ -0,0 +1,26 @@
+import React from 'react'
+let Header = () => {
+    return(
+            <header className='header'>
+        <div className="container text-center">
+            <div className="row align-items-start">
+              <div className="col header">
+                Одна из трех колонок
+              </div>
+              <div className="col header">
+                Одна из трех колонок
+              </div>
+              <div className="col header">
+              </div>
+              <div className="col header">
+                Одна из трех колонок
+              </div>
+                <div className="col header">
+                  Одна из трех колонок
+              </div>
+            </div>
+          </div>
+            </header>
+    );
+}
+export default Header;
