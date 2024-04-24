2f0f65bc10b7ea6b966032b889f38186e1b4440e
elf <tanya_zhukovskaya_96@mail.ru>
2024-04-25 03:14:02 +0500

    aaa

--- Front/JS/index.js
+++ Front/JS/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './code.js'
-import './style.css'
+import ReactDOM from 'react-dom/client';
+import App from './code.js';
+import './style.css';
+import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(<App />, document.getElementById('hheader'));
+const root = ReactDOM.createRoot(document.getElementById('hheader'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+); 
+
+reportWebVitals();
